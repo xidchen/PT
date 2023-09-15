c3c5b2b671abc729470574d22243fa09fdd0e2b3
Sheldon Chen <xidchen.work@gmail.com>
2023-09-15 10:27:25 +0800

    Optimize baichuan2_13b_chat_4bit.py

--- llm/baichuan2_13b_chat_4bit.py
+++ llm/baichuan2_13b_chat_4bit.py
@@ -25,3 +25,8 @@ model.generation_config = transformers.GenerationConfig.from_pretrained(
 messages = [{"role": "user", "content": "解释一下“温故而知新”"}]
 response = model.chat(tokenizer, messages)
 print(response)
+# "温故而知新"是一句中国古代名言，出自《论语·为政》篇。这句话的意思是回顾过去，从中汲取经验教训，
+# 从而更好地理解现在和未来的发展。具体来说，它鼓励人们在学习和生活中不断地回顾过去的事情，
+# 从中发现新的知识和见解，以便更好地应对当前的问题和挑战。
+# 这句话强调了回顾过去的重要性，因为我们可以从过去的经验中学习，从而更好地适应不断变化的环境。
+# 同时，它也提醒我们要保持开放的心态，对新事物和新观念保持敏感，以便在不断进步的社会中找到自己的定位。
